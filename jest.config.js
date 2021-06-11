52d96aa459ced5e3c695531a415f2c4bc8b06aed
antoine <antoine.guingand@wanadoo.fr>
2021-06-11 10:22:44 +0200

    update test config

--- jest.config.js
+++ jest.config.js
@@ -176,10 +176,9 @@ module.exports = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+    "/node_modules/(?!(prosemirror-markdown/src))"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
