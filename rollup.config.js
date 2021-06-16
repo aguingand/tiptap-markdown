519d1f5ffe7b5e6438a7803bebc7afb9188f872b
antoine <antoine.guingand@wanadoo.fr>
2021-06-16 20:46:04 +0200

    Update build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,43 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import babel from '@rollup/plugin-babel';
+import autoExternal from 'rollup-plugin-auto-external';
+
+const name = 'tiptap-markdown';
 
 export default {
     input: 'src/index.js',
-    output: {
-        file: 'dist/tiptap-markdown.es.js',
-        format: 'es'
-    },
+    output: [
+        {
+            name,
+            file: 'dist/tiptap-markdown.cjs.js',
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            name,
+            file: 'dist/tiptap-markdown.umd.js',
+            format: 'umd',
+            sourcemap: true,
+        },
+        {
+            name,
+            file: 'dist/tiptap-markdown.esm.js',
+            format: 'es',
+            sourcemap: true,
+        },
+    ],
     plugins: [
-        commonjs(),
         nodeResolve({
             preferBuiltins: false,
         }),
+        commonjs(),
         json(),
-    ],
-    external: [
-        'prosemirror-model',
-        '@tiptap/core',
-        'markdown-it',
+        babel({
+            babelHelpers: 'bundled',
+            exclude: 'node_modules/**',
+        }),
+        autoExternal(),
     ],
 }
