ad1b431e2f88dadfe4d5860ddc188f4048b5b4cf
antoine <antoine.guingand@wanadoo.fr>
2022-08-18 14:08:27 +0200

    Fix types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 
 
 import { Editor, EditorOptions, Node, Mark } from "@tiptap/core";
-import { Node as ProseMirrorNode } from "prosemirror-model";
 import { MarkdownSerializer, MarkdownSerializerState } from "prosemirror-markdown";
+import * as Prosemirror from "prosemirror-model";
 import * as MarkdownIt from "markdown-it";
 
 export type MarkdownEditorOptions = EditorOptions & {
@@ -13,37 +13,37 @@ export type MarkdownEditorOptions = EditorOptions & {
         bulletListMarker?: String,
         linkify?: String,
         breaks?: String,
-        extensions?: MarkdownExtension[],
+        extensions?: MarkdownExtension[]
     }
 }
 
 export type MarkdownExtension = {};
 export type MarkdownExtensionOptions = {
     parse?: {
-        setup(markdownit: MarkdownIt): void;
-        updateDOM(element: HTMLElement): void;
+        setup(markdownit: MarkdownIt): void,
+        updateDOM(element: HTMLElement): void
     },
     /**
      * @see MarkdownSerializer
      */
     serialize:
-        (state: MarkdownSerializerState, node: ProseMirrorNode, parent: ProseMirrorNode, index: number) => void
+        (state: MarkdownSerializerState, node: Prosemirror.Node, parent: Prosemirror.Node, index: number) => void
         | {
-            open: string | ((state: MarkdownSerializerState, mark: Mark, parent: ProseMirrorNode, index: number) => string),
-            close: string | ((state: MarkdownSerializerState, mark: Mark, parent: ProseMirrorNode, index: number) => string),
+            open: string | ((state: MarkdownSerializerState, mark: Prosemirror.Mark, parent: Prosemirror.Node, index: number) => string),
+            close: string | ((state: MarkdownSerializerState, mark: Prosemirror.Mark, parent: Prosemirror.Node, index: number) => string),
             mixable?: boolean,
             expelEnclosingWhitespace?: boolean,
             escape?: boolean
         },
 };
 
-export type MarkdownEditor = Editor & {
-    options: MarkdownEditorOptions
+export class MarkdownEditor extends Editor {
+    options: MarkdownEditorOptions;
     /**
      * Get the document as markdown
      */
     getMarkdown(): string;
-};
+}
 
 export function createMarkdownEditor(editor: typeof Editor): {
     new (options: Partial<MarkdownEditorOptions>): MarkdownEditor
