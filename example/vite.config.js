1d95c4dab9f790ea99821147fa06185b9be6c60b
antoine <antoine.guingand@wanadoo.fr>
2023-02-24 14:08:41 +0100

    Update example deps

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import analyzer from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()]
+    base: '/tiptap-markdown',
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            plugins: [
+                // analyzer(),
+            ]
+        }
+    }
 })
