c781d8aa1f0b7e8470f1c64917bed868f333bc4a
antoine <antoine.guingand@wanadoo.fr>
2023-11-01 14:46:18 +0100

    Migrate to vite + vitest

--- example/vite.config.js
+++ example/vite.config.js
@@ -3,18 +3,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import analyzer from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/tiptap-markdown',
     plugins: [vue()],
     resolve: {
         alias: {
             'tiptap-markdown': path.resolve(__dirname, '..'),
-        }
-    },
-    server: {
-        fs: {
-            strict: false
         }
     },
     build: {
@@ -23,5 +16,5 @@ export default defineConfig({
                 // analyzer(),
             ]
         }
-    }
+    },
 })
