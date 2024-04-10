067b595db39722eb97fe70798b3722dfc90ac54c
antoine <antoine.guingand@wanadoo.fr>
2024-04-10 22:10:28 +0200

    wip

---
+++ example-legacy/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import Editor from "./components/Editor.vue";
+</script>
+
+<template>
+    <div class="container">
+        <Editor />
+    </div>
+</template>
+
+<script>
+    import 'bootstrap/dist/css/bootstrap.css';
+</script>
+
+<style lang="scss" src="./app.scss">
+</style>
--- example/src/App.vue
+++ example/src/App.vue
@@ -1,16 +1,47 @@
-<script setup>
-    import Editor from "./components/Editor.vue";
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-    <div class="container">
-        <Editor />
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
     </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
-<script>
-    import 'bootstrap/dist/css/bootstrap.css';
-</script>
+<style scoped>
+header {
+  line-height: 1.5;
+}
 
-<style lang="scss" src="./app.scss">
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 </style>
