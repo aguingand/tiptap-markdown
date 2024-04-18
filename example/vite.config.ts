84fe88c65dd60de2ab8acb7292d0cd4dfffad446
antoine <antoine.guingand@wanadoo.fr>
2024-04-18 21:05:59 +0200

    wip

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,11 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
